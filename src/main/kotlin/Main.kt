b64c292ffcea2fd060c9cb4efee51800544959d9
YouDenisÑ <denis.botenkov@mail.ru>
2025-10-26 20:15:38 +0300

    Парсинг данных с сайта

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
+
 fun main() {
     val doc: Document =
         Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
