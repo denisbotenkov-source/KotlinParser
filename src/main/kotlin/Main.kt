4937870d9f294739b04d0aeac97a5d659a7a35f4
YouDenisÑ <denis.botenkov@mail.ru>
2025-10-26 20:09:09 +0300

    Парсинг данных с сайта

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 package org.example
 
-fun main(){
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
+
+fun main() {
+    val doc: Document =
+        Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
+            .get()
+    val quotes: Elements = doc.select("#sc-14uz67c-0")
+    println(quotes)
+    for (quote in quotes) {
+        println(quote.text())
+    }
 }
