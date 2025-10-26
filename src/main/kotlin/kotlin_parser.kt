f294aaed09b7e17ed67504f6aeb6020e60cef8c7
YouDenisÑ <denis.botenkov@mail.ru>
2025-10-26 21:35:20 +0300

    Парсинг данных с сайта

---
+++ src/main/kotlin/kotlin_parser.kt
@@ -0,0 +1,16 @@
+package org.example
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+fun main() {
+    val doc: Document =
+        Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
+            .get()
+    val quotes: Elements = doc.select("#sc-14uz67c-0")
+    println(quotes)
+    for (quote in quotes) {
+        println(quote.text())
+    }
+}
